54ec75b0ecc754d442b3908b078049963ca69381
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2021-06-22 11:31:45 +0530

    started basic of flutter

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home:Scaffold(
+    appBar: AppBar(
+      title: Text('my first app'),
+      centerTitle: true,
+    ),
+    body:Center(
+      child: Text('hello world'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Text('click'),
+    )
+  )
+  
+));
+
+
